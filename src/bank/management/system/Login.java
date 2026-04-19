aa2c62c8ca02bde349ac2cac552f91e6bf0c5e7b
Akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-04-19 14:39:35 +0530

    Initial commit - Bank Management System

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+import javax.swing.*;
+
+public class Login extends JFrame{
+    
+    Login(){
+         setSize(800, 480);
+         setVisible(true);
+         setTitle("AUTOMATED TELLER MACHINE");
+         setLocation(350, 200);
+    }
+    public static void main(String args[]){
+          new Login();
+    }
+    
+}
